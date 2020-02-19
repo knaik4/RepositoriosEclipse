803a6fa25e9500f8136244a46e65b4266537ea48
kevin <kevin@192.168.1.157>
2020-02-19 15:45:13 +0100

    le agregue un for

--- src/Test/HolaMundo.java
+++ src/Test/HolaMundo.java
@@ -2,6 +2,8 @@ package Test;
 
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
 	}
 }
